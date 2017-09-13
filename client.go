e2ab0e8bb075628ef483711c9959f1b52e4f2dd6
Vasilyuk Vasiliy <vasilyukvasiliy@gmail.com>
2017-09-13 22:47:18 +0400

    feat(*): Created initial client

--- client.go
+++ client.go
@@ -1,8 +1,49 @@
 package blockchain
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 // Go client blockchain.info api
 
 const (
 	apiRootTor = "https://blockchainbdgpzk.onion"
 	apiRootNet = "https://blockchain.info"
 )
+
+type Client struct {
+	*http.Client
+	ApiRoot string
+}
+
+func (c *Client) doRequest(path string, i interface{}) (e error) {
+	fullPath := c.ApiRoot + path
+
+	response, e := c.Get(fullPath)
+	if e != nil {
+		return
+	}
+
+	defer response.Body.Close()
+
+	bytes, e := ioutil.ReadAll(response.Body)
+	if e != nil {
+		return
+	}
+	if response.Status[0] != '2' {
+		return fmt.Errorf("Response error status %3s: %s", response.Status, string(bytes))
+	}
+
+	return json.Unmarshal(bytes, &i)
+}
+
+func New() *Client {
+	return &Client{Client: &http.Client{}, ApiRoot: apiRootNet}
+}
+
+func NewTor() *Client {
+	return &Client{Client: &http.Client{}, ApiRoot: apiRootTor}
+}
