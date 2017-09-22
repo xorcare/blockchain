bd13f303268185eca1644814b18689c10c70ac18
Vasilyuk Vasiliy <vasilyukvasiliy@gmail.com>
2017-09-22 20:12:21 +0400

    feat(unspent): Added methods api unspent outputs

---
+++ unspent.go
@@ -0,0 +1,30 @@
+package blockchain
+
+import "strings"
+
+type UnspentOutputs struct {
+	Notice         string           `json:"notice,omitempty"`
+	UnspentOutputs []*UnspentOutput `json:"unspent_outputs"`
+}
+
+type UnspentOutput struct {
+	TxAge     string `json:"tx_age"`
+	TxHash    string `json:"tx_hash"`
+	TxIndex   uint64 `json:"tx_index"`
+	TxOutputN uint64 `json:"tx_output_n"`
+	Script    string `json:"script"`
+	Value     int64  `json:"value"`
+}
+
+func (c *Client) GetUnspent(addresses []string, params ...map[string]string) (response *UnspentOutputs, e error) {
+	options := map[string]string{"active": strings.Join(addresses, "|")}
+	if len(params) > 0 {
+		for k, v := range params[0] {
+			options[k] = v
+		}
+	}
+	response = &UnspentOutputs{}
+	e = c.DoRequest("/unspent", response, options)
+
+	return
+}
---
+++ unspent_test.go
@@ -0,0 +1,35 @@
+package blockchain
+
+import "testing"
+
+func TestGetUnspent(t *testing.T) {
+	addresses := []string{
+		"1J6PYEzr4CUoGbnXrELyHszoTSz3wCsCaj",
+		"12cbQLTFMXRnSzktFkuoG3eHoMeFtpTu3S",
+		"15yN7NPEpu82sHhB6TzCW5z5aXoamiKeGy",
+		"1dyoBoF5vDmPCxwSsUZbbYhA5qjAfBTx9",
+		"1PYELM7jXHy5HhatbXGXfRpGrgMMxmpobu",
+		"17abzUBJr7cnqfnxnmznn8W38s9f9EoXiq",
+		"1DMGtVnRrgZaji7C9noZS3a1QtoaAN2uRG",
+		"1CYG7y3fukVLdobqgUtbknwWKUZ5p1HVmV",
+	}
+
+	response, e := New().GetUnspent(addresses, map[string]string{"limit": "1000"})
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	for _, v := range response.UnspentOutputs {
+		if len(v.TxHash) != 64 {
+			t.Fatal("Wrong length value on field 'TxHash'")
+		}
+
+		if v.TxIndex < 1 {
+			t.Fatal("Wrong value on field 'TxIndex'")
+		}
+
+		if v.Script == "" {
+			t.Fatal("Wrong count items on field 'Script'")
+		}
+	}
+}
